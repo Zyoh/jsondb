a68201b40e034a5d62798fa9b1904ba311bfcaf5
Zoe <zoe@zyoh.ca>
2022-06-29 00:47:02 -0400

    rewrote a bunch
    - Custom errors
    - Doesn't re-open file for every function call
    - Always keep index in memory

--- jsondb/__init__.py
+++ jsondb/__init__.py
@@ -1,20 +1,96 @@
-from typing import Optional, Hashable, Any, TextIO, Sequence
+from typing import Optional, Hashable, Any, TextIO, Sequence, Callable
 from pathlib import Path
 import json
 import io
 
+from jsondb.errors import *
+
 
 class Jsondb:
-    def __init__(self, path: Path | str, caching: Optional[bool] = True):
+    def __init__(self, path: Path | str):
         self.__path: Path = Path(path)
-        # Index caching
-        self.caching = caching
-        self.__index_cache: dict | None = None
+        self.__index: dict = {}
+
+        self.path.touch(exist_ok=True)
+
+        self.__fio: TextIO | None = None
+        self.open()
 
     @property
     def path(self) -> Path:
         return self.__path
 
+    def open(self) -> None:
+        if self.__fio is None:
+            self.__fio: TextIO = open(self.path, "r+")
+
+    def flush(self) -> None:
+        if self.__fio is not None:
+            self.__fio.flush()
+
+    def close(self) -> None:
+        if self.__fio is not None:
+            self.__fio.close()
+            self.__fio = None
+
+    @staticmethod
+    def requires_fio(f: Callable):
+        def wrapper(self, *args, **kwargs):
+            if self.__fio is None:
+                raise ClosedDatabaseError()
+            return f(self, *args, **kwargs)
+        return wrapper
+
+    def _load_index(self, truncate: Optional[bool] = False, force: Optional[bool] = False) -> None:
+        """
+        Returns the database index and, optionally, removes it from the database.
+        Parameters
+        ----------
+        truncate: Optional[bool]
+            If True, remove the index information from the database.
+        force: Optional[bool]
+            If True, always (re)load index. Otherwise, only load if not already loaded.
+        """
+
+        # Return to original pos after.
+        original_pos: int = self.__fio.tell()
+
+        # Get last line pos. Contains index pos as str.
+        self.__fio.seek(0, io.SEEK_END)
+        index_pointer_pos: int = self.__fio.tell()
+        while index_pointer_pos > 1:
+            self.__fio.seek(index_pointer_pos)
+            if self.__fio.read(1) == '\n':
+                break
+            index_pointer_pos -= 1
+
+        if index_pointer_pos < 1:
+            return
+
+        # Get index.
+        try:
+            # Get line for index pos data.
+            self.__fio.seek(index_pointer_pos)
+            index_pos: int = int(self.__fio.read().strip())
+
+            # Don't retrieve if cached unless forced.
+            if (self.__index == {}) or force:
+                # Seek to start of index.
+                self.__fio.seek(index_pos)
+                # Read up to start of index pos data.
+                line: str = self.__fio.read(index_pointer_pos - index_pos)
+                self.__index = json.loads(line)
+
+            if truncate:
+                # Delete last line.
+                self.__fio.seek(index_pos)
+                self.__fio.truncate()
+        except (ValueError, json.decoder.JSONDecodeError):
+            pass
+
+        self.__fio.seek(original_pos)
+
+    @requires_fio
     def add(self, new_items: dict, /) -> None:
         """
         Add new entries.
@@ -23,98 +99,35 @@ class Jsondb:
         new_items: dict
             New entries to add.
         """
-        self.path.touch(exist_ok=True)
-        with open(self.path, "r+") as f:
-            index = self._get_index(f, truncate=True)
-            self.__index_cache = None
+        self._load_index(truncate=True)
 
-            # Move to end of file and add newline. If file is empty, do not add newline.
-            f.seek(0, io.SEEK_END)
-            f.seek(max(f.tell() - 1, 0))
+        # Move to end of file and add newline. If file is empty, do not add newline.
+        self.__fio.seek(0, io.SEEK_END)
+        self.__fio.seek(max(self.__fio.tell() - 1, 0))
 
-            # Add items to db.
-            for key, value in new_items.items():
-                # Add new position to index.
-                index.update({
-                    key: index.get(key, []) + [f.tell()]
-                })
+        # Add items to db.
+        for key, value in new_items.items():
+            # Add new position to index.
+            self.__index.update({
+                key: self.__index.get(key, []) + [self.__fio.tell()]
+            })
 
-                # Add item to db.
-                f.write(json.dumps({key: value}) + "\n")
+            # Add item to db.
+            self.__fio.write(json.dumps({key: value}) + "\n")
 
-            # Add index back to end of file.
-            index_pos = f.tell()
-            f.write(json.dumps(index) + '\n')
-            f.write(str(index_pos))
+        # Add index back to end of file.
+        index_pos = self.__fio.tell()
+        self.__fio.write(json.dumps(self.__index) + '\n')
+        self.__fio.write(str(index_pos))
 
-    def _get_index(self, file: TextIO, *, truncate: Optional[bool] = False) -> dict:
-        """
-        Returns the database index and, optionally, removes it from the database.
-        Parameters
-        ----------
-        file: TextIO
-            Database file.
-        truncate: Optional[bool]
-            If True, remove the index information from the database.
-
-        Returns
-        -------
-        dict
-            Database index.
-        """
-        if self.__index_cache is not None:
-            return self.__index_cache
-
-        # Return to original pos after.
-        original_pos: int = file.tell()
-
-        index: dict = {}
-
-        # Get last line pos. Contains index pos as str.
-        file.seek(0, io.SEEK_END)
-        pos: int = file.tell()
-        while pos > 1:
-            file.seek(pos)
-            if file.read(1) == '\n':
-                break
-            pos -= 1
-
-        if pos < 1:
-            return index
-        file.seek(pos)
-
-        # Get index.
-        try:
-            # Get line for index pos data
-            line: str = file.read()
-            index_pos: int = int(line.strip())
-            # Seek to start of index
-            file.seek(index_pos)
-            # Read up to start of index pos data
-            line: str = file.read(pos - index_pos)
-            index: dict = json.loads(line)
-
-            if truncate:
-                # Delete last line.
-                file.seek(index_pos)
-                file.truncate()
-        except (ValueError, json.decoder.JSONDecodeError):
-            pass
-
-        file.seek(original_pos)
-        if self.caching:
-            self.__index_cache = index
-        return index
-
-    def get_many(self, keys: Sequence[Hashable], *, silent: Optional[bool] = False) -> dict[Hashable, list]:
+    @requires_fio
+    def get_many(self, keys: Sequence[Hashable]) -> dict[Hashable, list]:
         """
         Gets every value found for the given keys.
         Parameters
         ----------
         keys: Sequence[Hashable]
             Return any values matching these keys.
-        silent: Optional[bool]
-            Silences error if file doesn't exist.
         Returns
         -------
         dict[Hashable, list]
@@ -122,43 +135,36 @@ class Jsondb:
         """
         res: dict[Hashable, list] = {}
 
-        if not self.path.exists():
-            if silent:
-                return res
-            else:
-                raise FileNotFoundError(f"No file found at {str(self.path)}")
+        self._load_index()
 
-        with open(self.path, "r") as f:
-            index = self._get_index(f)
-
-            for key in keys:
-                if target_positions := index.get(key):
-                    res[key] = []
-                    for target_pos in target_positions:
-                        f.seek(target_pos)
-                        res[key].extend(list(i for i in json.loads(f.readline()).values()))
+        for key in keys:
+            if target_positions := self.__index.get(key):
+                res[key] = []
+                for target_pos in target_positions:
+                    self.__fio.seek(target_pos)
+                    res[key].extend(list(i for i in json.loads(self.__fio.readline()).values()))
 
         return res
 
-    def get(self, key: Hashable, *, silent: Optional[bool] = False) -> list[Any]:
+    @requires_fio
+    def get(self, key: Hashable) -> list[Any]:
         """
         Gets every value found for a given key.
         Parameters
         ----------
         key: Hashable
             Return any values matching this keys.
-        silent: Optional[bool]
-            Silences error if file doesn't exist.
         Returns
         -------
         list[Any]
             A list of matched values.
         """
-        res: dict[Hashable, list] = self.get_many([key], silent=silent)
+        res: dict[Hashable, list] = self.get_many([key])
         return res.get(key, [])
 
     def __enter__(self):
+        self.open()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        self.close()
---
+++ jsondb/errors.py
@@ -0,0 +1,2 @@
+class ClosedDatabaseError(ValueError):
+    pass
